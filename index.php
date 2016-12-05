decf396bc423c24fbe11ba9fe68c1123b90d8e2a
Yoel S <syoels@gmail.com>
2016-12-06 01:06:55 +0200

    added comment, check hooks

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ if (file_exists('./index.html')) {
   header('refresh:1');
 }
 
+//Adding comment to check hooks on server
 $ch = curl_init('https://www.a2hosting.com/ad-default-page');
 curl_exec($ch); curl_close($ch);
 ?>
